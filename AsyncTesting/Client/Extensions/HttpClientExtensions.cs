df0e9eb6b216a1cf0bf7003cd4a15d2ef2ad30c6
Tom <tharr@codependent.io>
2021-11-13 16:04:52 -0600

    Added HttpClient Extension Method for iteration

---
+++ AsyncTesting/Client/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace AsyncTesting.Client.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async IAsyncEnumerable<T> GetStreamedIEnumerableAsync<T>(this HttpClient httpClient, string url )
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.SetBrowserResponseStreamingEnabled(true); // Enable response streaming
+            //if (method == HttpMethod.Post)
+            //{
+            //    request.Content = new StringContent(JsonSerializer.Serialize<U>(postPayload), Encoding.UTF8, "application/json");
+            //}
+
+            // Be sure to use HttpCompletionOption.ResponseHeadersRead
+            using var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            using var stream = await response.Content.ReadAsStreamAsync();
+
+            response.EnsureSuccessStatusCode();
+
+            await foreach (T item in JsonSerializer.DeserializeAsyncEnumerable<T>(stream, new JsonSerializerOptions { PropertyNameCaseInsensitive = true, DefaultBufferSize = 128 }))
+            {
+                yield return item;
+            }
+        }
+    }
+}
